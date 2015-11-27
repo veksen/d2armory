a6a2929a15223d1f75b5ce233d9ea27095f9225c
veksen <jp@veksen.net>
2015-11-27 01:25:14 -0500

    move class list as vue data

---
+++ js/vue-script.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#planner',
+
+    data: {
+        classes: [
+            {
+                short: 'zon',
+                long: 'Amazon'
+            },
+            {
+                short: 'sin',
+                long: 'Assassin'
+            },
+            {
+                short: 'bar',
+                long: 'Barbarian'
+            },
+            {
+                short: 'dru',
+                long: 'Druid'
+            },
+            {
+                short: 'pal',
+                long: 'Paladin'
+            },
+            {
+                short: 'nec',
+                long: 'Necromancer'
+            },
+            {
+                short: 'sor',
+                long: 'Sorceress'
+            }
+        ]
+    }
+});
